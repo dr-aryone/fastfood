50f012985856b24a7782895b1142030b555db165
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 15:51:30 +0000

    Deploy christopherkade/fastfood to github.com/christopherkade/fastfood.git:gh-pages

---
+++ precache-manifest.19951cf87cebdcbdaac3cfea723663af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01130b5539780a5bccb0f2f42d0184b6",
+    "url": "/fastfood/index.html"
+  },
+  {
+    "revision": "889fe1a32377e64d7793",
+    "url": "/fastfood/static/css/main.0f4e07a3.chunk.css"
+  },
+  {
+    "revision": "caa52e58b5df270d2e91",
+    "url": "/fastfood/static/js/2.bbd8cc3b.chunk.js"
+  },
+  {
+    "revision": "889fe1a32377e64d7793",
+    "url": "/fastfood/static/js/main.a926117a.chunk.js"
+  },
+  {
+    "revision": "880e8fd10bd961d635ff",
+    "url": "/fastfood/static/js/runtime~main.c185cc8e.js"
+  }
+]);
